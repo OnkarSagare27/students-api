4569f46a3bf55a025b9b334b76e93cdd2c2f95b5
OnkarSagare27 <onkardsagare@gmail.com>
2025-03-09 12:00:00 +0530

    add request validation

---
+++ internal/http/handlers/student/student.go
@@ -0,0 +1,36 @@
+package student
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+
+	"github.com/OnkarSagare27/students-api/internal/types"
+	response "github.com/OnkarSagare27/students-api/internal/utils"
+	"github.com/go-playground/validator/v10"
+)
+
+func New() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var student types.Student
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return
+		}
+
+		if err := validator.New().Struct(student); err != nil {
+			validateErrs := err.(validator.ValidationErrors)
+
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateErrs))
+		}
+		response.WriteJson(w, http.StatusCreated, map[string]any{"success": true})
+
+	}
+}
